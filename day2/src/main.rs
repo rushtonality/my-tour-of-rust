9b9860e26850f2150b1b422202cc6a2fbdb37568
Stephen Rushton <rushtonality@gmail.com>
2019-01-14 09:28:02 -0500

    Optimize code so that it actually finishes.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,17 +1,23 @@
-fn is_prime(number : i64) -> bool {
-    for candidate in 2..=(number/2) {
+pub fn is_prime(number : i64) -> bool {
+    let mut candidate : i64 = 2;
+    let mut limit = number / 2;
+
+    while candidate <= limit {
         if number % candidate == 0 {
             return false;
         }
+        limit = number / candidate;
+        candidate += 1;
     }
     true
 }
 
-fn largest_prime_factor(number : i64) -> i64 {
+pub fn largest_prime_factor(number : i64) -> i64 {
     let mut prime = 1;
-    for factor in 1..number {
-        if number % factor == 0 && is_prime(factor){
-            prime = factor
+    for factor in 2..number {
+        if number % factor == 0 && is_prime(number / factor) {
+            prime = number / factor;
+            break;
         }
     }
     prime
@@ -19,8 +25,14 @@ fn largest_prime_factor(number : i64) -> i64 {
 
 fn main() {
     let number = 13195;
+    // Largest prime factor of 13195: 29
     println!("Largest prime factor of {}: {}", number, largest_prime_factor(number));
 
-    let number = 600851475143;
+    let number : i64 = 60085143;
+    // Largest prime factor of 60085143: 13597
+    println!("Largest prime factor of {}: {}", number, largest_prime_factor(number));
+
+    let number : i64 = 600851475143;
+    // Largest prime factor of 600851475143: 6857
     println!("Largest prime factor of {}: {}", number, largest_prime_factor(number));
 }
