d9aca26f2e42882d3a6e1c9f24ffb92858212b9f
Stephen Rushton <rushtonality@gmail.com>
2019-01-14 09:53:35 -0500

    Add unit testing to day2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -46,3 +46,53 @@ fn main() {
     // Largest prime factor of 600851475143: 6857
     println!("Largest prime factor of {}: {}", number, largest_prime_factor(number));
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prime_1() {
+        assert_eq!(true, is_prime(1));
+    }
+    #[test]
+    fn test_prime_2() {
+        assert_eq!(true, is_prime(2));
+    }
+    #[test]
+    fn test_prime_3() {
+        assert_eq!(true, is_prime(3));
+    }
+    #[test]
+    fn test_prime_4() {
+        assert_eq!(false, is_prime(4));
+    }
+    #[test]
+    fn test_prime_5() {
+        assert_eq!(true, is_prime(5));
+    }
+    #[test]
+    fn test_prime_large() {
+        assert_eq!(true, is_prime(67_280_421_310_721));
+    }
+    
+    #[test]
+    fn test_largest_prime_factor_13195() {
+        let number = 13195;
+        let answer = 29;
+        assert_eq!(answer, largest_prime_factor(number));
+    }
+    #[test]
+    fn test_largest_prime_factor_60085143() {
+        let number = 60085143;
+        let answer = 13597;
+        assert_eq!(answer, largest_prime_factor(number));
+    }
+    #[test]
+    fn test_largest_prime_factor_600851475143() {
+        let number = 600851475143;
+        let answer = 6857;
+        assert_eq!(answer, largest_prime_factor(number));
+    }
+}
